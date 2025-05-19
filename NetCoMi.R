3c294a09859d02cff4c14897beadad2acf39a086
XymeV <xymenavaz@gmail.com>
2025-05-19 15:49:17 -0600

    paquete NetCoMi

--- NetCoMi.R
+++ NetCoMi.R
@@ -2,6 +2,15 @@
 
 ### Instalar paquete
 
-install.packages("BiocMananger")
+install.packages("devtools")
+install.packages("BiocManager")
 
-BiocManager::install("NetCoMi")
+# Since two of NetCoMi's dependencies are only available on GitHub, 
+# it is recommended to install them first:
+devtools::install_github("zdk123/SpiecEasi")
+devtools::install_github("GraceYoon/SPRING")
+
+# Install NetCoMi
+devtools::install_github("stefpeschel/NetCoMi", 
+                         repos = c("https://cloud.r-project.org/",
+                                   BiocManager::repositories()))
