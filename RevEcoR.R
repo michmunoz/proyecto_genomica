70a256e0df336fa7daecd9200d5c52e0b2a72440
XymeV <xymenavaz@gmail.com>
2025-05-24 20:25:53 -0600

    actualizacion paquete

--- RevEcoR.R
+++ RevEcoR.R
@@ -1,7 +1,5 @@
 
 
 ###Instalar el paquete 
-if (!require(devtools) 
-    install.packages("devtools") 
-    devtools::install_github("yiluheihei/RevEcoR") 
+install.packages("RevEcoR")
 library(RevEcoR)
